a77132b26ab52c677443dcd109158d2afd4fd3c7
aljo252 <alexjohnston252@live.co.uk>
2017-06-16 17:26:15 +0100

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -9,7 +9,7 @@ png("plot3.png", width=400, height=400)
 plot(data2$Time, data2$Sub_metering_1, type="l", col="black",
      xlab="", ylab="Energy sub metering")
 lines(data2$Time, data$Sub_metering_2, col="red")
-lines(data2$Time, data22$Sub_metering_3, col="blue")
+lines(data2$Time, data2$Sub_metering_3, col="blue")
 legend("topright",
        col=c("black", "red", "blue"),
        c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
